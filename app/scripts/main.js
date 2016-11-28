a4fc3cbd83a3190235bc691a057f321a3739bb24
AXM DVR <axm.dvr@gmail.com>
2016-11-27 23:52:19 -0500

    Ajouts v.0.2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-window.console.log('\'Allo \'Allo.!');
+window.console.log('OK!');
+
+var moment;
+
+/* - */
+(function () {
+    'use strict';
+
+    // ---------------------------------------------------------------------------------------
+    var datedujour = document.querySelector('.date'), maintenant = moment();
+
+    window.console.log('moment().format(\'LLLL\') : ' + maintenant.format('LLLL'));
+    datedujour.innerHTML = maintenant.format('LLLL') + '.';
+}());
